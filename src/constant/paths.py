6bcbc558821d56f9818f03477d3def1d59bf98d4
alekseyIsakin <karahunforever@gmail.com>
2022-03-04 13:21:41 +0800

    upd

--- src/constant/paths.py
+++ src/constant/paths.py
@@ -8,6 +8,7 @@ PATH_TO_SRC = ""
 
 PATH_TO_INPUT_JPG = r"images\input\test2.png"
 PATH_TO_INPUT_ = r"images\input\\"
+PATH_TO_OUTPUT_ = r"images\ourput\\"
 # PATH_TO_INPUT_JPG = r"images\input\input.jpg"
 PATH_TO_OUTPUT_JPG = r"images\output\output.jpg"
 PATH_TO_ISLANDS_JPG = r"images\output\islands.png"
