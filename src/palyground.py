f97a07fd0b51084496ab2e3b51f87ea701e71442
alekseyIsakin <karahunforever@gmail.com>
2022-03-06 12:41:24 +0800

    upd

--- src/palyground.py
+++ src/palyground.py
@@ -19,16 +19,16 @@ from constant.paths import PATH_TO_IMAGES, PATH_TO_INPUT_JPG, \
                   PATH_TO_OUTPUT_
 
 import pickle
+import timeit
+# lg.info("palyground start")
+# file = "input.jpg"
+# img:ndarray     = cv2.imread(PATH_TO_INPUT_ + file, cv2.IMREAD_GRAYSCALE)
+# img = get_mask_from_gray(img, upper_val=100)
+# lg.info(f"load image {img.shape}")
 
-lg.info("palyground start")
-file = "input.jpg"
-img:ndarray     = cv2.imread(PATH_TO_INPUT_ + file, cv2.IMREAD_GRAYSCALE)
-img = get_mask_from_gray(img, upper_val=100)
-lg.info(f"load image {img.shape}")
-
-lines_arr = get_lines(get_mask_from_gray(img).copy())
-complete = islands_from_lines(lines_arr)
-lg.info(f"get islands {len(complete)}")
+# lines_arr = get_lines(get_mask_from_gray(img).copy())
+# complete = islands_from_lines(lines_arr)
+# lg.info(f"get islands {len(complete)}")
 
 # with open("test.dt", mode="wb") as f:
 #   pickle.dump(complete, f, protocol=pickle.HIGHEST_PROTOCOL)
@@ -36,7 +36,28 @@ lg.info(f"get islands {len(complete)}")
 # with open("test.dt", mode="rb") as f:
 #   complete = pickle.load(f)
 
-isl = draw_islands(complete, cv2.cvtColor(img, cv2.COLOR_GRAY2BGR))
+# isl = draw_islands(complete, cv2.cvtColor(img, cv2.COLOR_GRAY2BGR))
 
-imwrite(PATH_TO_OUTPUT_ + "islands.png", isl)
-lg.info(f"fin")
+# imwrite(PATH_TO_OUTPUT_ + "islands.png", isl)
+# lg.info(f"fin")
+
+def test(isl:Island, isl2:Island):
+  isl = isl + isl2
+
+isl = Island()
+isl2 = Island()
+
+cnt = 10
+l = [Line(rnd.randint(0,100),rnd.randint(0,100),rnd.randint(100,200)) for i in range(cnt)]
+isl += l
+
+cnt = 10
+l = [Line(rnd.randint(0,100),rnd.randint(0,100),rnd.randint(100,200)) for i in range(cnt)]
+isl2 += l
+
+setup="""
+from __main__ import test, isl, isl2
+"""
+
+tm = timeit.timeit('test(isl, isl2)', setup=setup, number=100)
+print(tm)
